d5538e795effc6574c3081243492aac483e524fe
liyang <liyangcoder@163.com>
2018-06-02 21:32:33 +0800

    通过start.sh和restart.sh启动的服务器,自动在localhost下创建index.php文件

--- restart.sh
+++ restart.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+./start.sh
+
 docker-compose restart
--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+DIRECTORY=www/localhost
+if [ "`ls -A $DIRECTORY`" = "" ];
+then
+cat>www/localhost/index.php<<EOF
+<?php
+phpinfo();
+EOF
+fi
+
 docker-compose up -d
